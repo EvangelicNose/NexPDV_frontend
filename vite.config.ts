b2e0543f5529541517bcda4a1ea0b583eddf34df
Samuel Amorim <samufamorim@gmail.com>
2026-08-31 21:27:07 -0300

    feat: set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/NexPDV_frontend/',
   server: { port: 3000 },
   preview: { port: 3000 },
 })
